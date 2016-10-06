e46895418271d39e4357df2bdc277183a35a0f86
gomezportillo <pedromanuel.gomezportillo@alu.uclmes>
2016-10-06 18:15:32 +0200

    first commit

---
+++ parserXML2CSV.py
@@ -0,0 +1,21 @@
+""" GIPUZKOA
+"""
+
+import codecs
+
+class Parser():
+
+    def __init__(self, fin):
+        self.file_in = fin
+        self.file_out = fout
+
+    def parse(self):
+        with open(self.file) as fin:
+            with open
+            for linea in f:
+                if "incidenciaGeolocalizada" in linea:
+                    print (linea)
+
+
+p = Parser("inc2006.xml", "inc2006.csv")
+p.parse()
