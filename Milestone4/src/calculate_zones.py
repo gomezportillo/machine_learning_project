0c67101fa8985d107666892b5a2e2dd693025657
gomezportillo <pedromanuel.gomezportillo@alu.uclmes>
2016-11-03 18:55:58 +0100

    milestone4. adding zones to csv file

---
+++ Milestone4/src/calculate_zones.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@author: Andrés Montoro Montarroso
+@author: Pedro Manuel Gómez-Portillo
+"""
+
+from sklearn.cluster import DBSCAN
+
+# Calculating labels my means of the DBSCAN algorithm
+X = []
+with open ('data/inc2006.csv','r') as file_filter:
+    for line in file_filter:
+        cadena = line.split(";")
+        data=[cadena[2],cadena[12]]
+        X.append(data)
+labels = DBSCAN(eps=0.0075, min_samples=5).fit_predict(X)
+
+# Appending the id of the zone to each incident
+nl = ""
+with open('data/inc2006.csv') as fin:
+    with open('data/inc2006zone.csv', 'w') as fout:
+        lines = fin.readlines()
+        for i in xrange(len(lines)):
+            nl = str(lines[i].replace("\n", "")) + ";" + str(labels[i]) + "\n"
+            fout.write(nl)
---
+++ Milestone4/src/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@author: Andrés Montoro Montarroso
+@author: Pedro Manuel Gómez-Portillo
+"""
+
+"""
+Tasks to achieve:
+    * Calculate number of accidents per zone
+    * 
